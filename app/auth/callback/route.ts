b4b04c4604330eb63d14a28753ec8e5fe274e069
Ravindra Choudhary <ravindra.viklele@gmail.com>
2026-02-19 14:27:30 +0530

    feat: Implement Supabase authentication callback route to handle code exchange and redirection.

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,16 +4,17 @@ import { createClient } from "@/utils/supabase/server";
 import { getURL } from "@/utils/get-url";
 
 export async function GET(request: Request) {
-    const { searchParams, origin } = new URL(request.url);
+    const { searchParams } = new URL(request.url);
     const code = searchParams.get("code");
     // if "next" is in search params, use it as the redirection URL
     const next = searchParams.get("next") ?? "/";
+
+    const origin = getURL();
 
     if (code) {
         const supabase = await createClient();
         const { error } = await supabase.auth.exchangeCodeForSession(code);
         if (!error) {
-            const origin = getURL();
             return NextResponse.redirect(`${origin}${next}`);
         }
     }
